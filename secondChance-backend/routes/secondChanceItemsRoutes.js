3c08c0f30314e99ed78af68c5e9745f7a3d90a43
RoterHund <foxton.alan@gmail.com>
2024-04-04 16:29:55 +0200

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -11,10 +11,10 @@ const directoryPath = 'public/images'
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, directoryPath); // Specify the upload directory
-  }
+  },
   filename: function (req, file, cb) {
     cb(null, file.originalname); // Use the original file name
-  }
+  },
 })
 
 const upload = multer({ storage: storage })
