61c338c3973defdd8196d7e5c304e2c476c0b527
RoterHund <foxton.alan@gmail.com>
2024-04-04 16:39:57 +0200

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -27,8 +27,7 @@ router.get('/', async (req, res, next) => {
     const collection = db.collection('secondChanceItems')
     const secondChanceItems = await collection.find({}).toArray()
     res.json(secondChanceItems)
-  }
-  catch (e) {
+  }  catch (e) {
     logger.console.error('oops something went wrong', e)
     next(e)
   }
@@ -37,16 +36,15 @@ router.get('/', async (req, res, next) => {
 // Add a new item
 router.post('/', upload.single('file'), async (req, res, next) => {
   try {
-    const itemId = req.params.id
     const db = await connectToDatabase()
-    const collection = db.collection("secondChanceItems")
+    const collection = db.collection('secondChanceItems')
     let secondChanceItem = req.body
-    const lastId = await collection.find().sort({ 'id': -1 }).limit(1)
+    const lastId = await collection.find().sort({ id: -1 }).limit(1)
     await lastId.forEach(item => {
       secondChanceItem.id = (parseInt(item.id) + 1).toString()
-    });
-    const date_added = Math.floor(new Date().getTime() / 1000)
-    secondChanceItem.date_added = date_added
+    })
+    const dateAdded = Math.floor(new Date().getTime() / 1000)
+    secondChanceItem.date_added = dateAdded
     res.status(201).json(secondChanceItem.ops[0])
     secondChanceItem = await collection.insertOne(secondChanceItem)
   } catch (e) {
@@ -62,7 +60,7 @@ router.get('/:id', async (req, res, next) => {
     const collection = db.collection('secondChanceItems')
     const secondChanceItem = await collection.findOne({ id: itemId })
     if (!secondChanceItem) {
-      return res.status(404).send("secondChanceItem not found")
+      return res.status(404).send('condChanceItem not found')
     }
     res.json(secondChanceItem)
   } catch (e) {
@@ -79,7 +77,7 @@ router.put('/:id', async (req, res, next) => {
     const secondChanceItem = await collection.findOne({ id: itemId })
     if (!secondChanceItem) {
       logger.error('secondChanceItem not found')
-      return res.status(404).json({ error: "secondChanceItem not found" })
+      return res.status(404).json({ error: 'secondChanceItem not found' })
     }
     secondChanceItem.category = req.body.category
     secondChanceItem.condition = req.body.condition
@@ -89,7 +87,7 @@ router.put('/:id', async (req, res, next) => {
     secondChanceItem.updatedAt = new Date()
 
     const updatepreloveItem = await collection.findOneAndUpdate(
-      { id },
+      { id: secondChanceItem.id },
       { $set: secondChanceItem },
       { returnDocument: 'after' }
     )
